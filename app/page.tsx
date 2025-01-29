08fe27a755ab44c9549a3254724c7a84d95abb5d
이원빈 <2wonbin@kakao.com>
2025-01-30 01:07:02 +0900

    💄  전역 스타일 및 레이아웃 구성 추가: TailwindCSS 및 기본 레이아웃 파일 생성

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <h1 className="font-bold text-2xl">첫 커밋</h1>
+        </div>
+    );
+}
